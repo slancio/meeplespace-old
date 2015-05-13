f7c30f6000057b65ed9a3b310c5c3db81f67052e
Eric Savadian <eric.j.savadian@gmail.com>
2015-05-13 13:48:53 -0400

    Run rails g backbone:install --javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,11 @@
 //= require underscore
 //= require backbone
 //= require backbone.marionette
+//= require meeple_space
+// = require meeplespace
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
---
+++ app/assets/javascripts/meeplespace.js
@@ -0,0 +1,15 @@
+// window.Meeplespace = {
+//   Models: {},
+//   Collections: {},
+//   Views: {},
+//   Routers: {},
+//   initialize: function() {
+//     alert('Hello from Backbone!');
+//   }
+// };
+
+// $(document).ready(function(){
+//   Meeplespace.initialize();
+// });
+
+// commenting out generated Backbone app js for now.
