71abce701352d95570869f64d4d2f4aa8b9991c2
Eric Savadian <eric.j.savadian@gmail.com>
2015-05-13 17:49:57 -0400

    Commit last changes to branch: Marionette is not working and it is time to ditch it.

--- app/assets/javascripts/meeple_space.js
+++ app/assets/javascripts/meeple_space.js
@@ -2,13 +2,65 @@
   "use strict";
 
   var App = Backbone.Marionette.Application.extend({
-
+    setRootLayout: function () {
+      this.rootView =  new MeepleSpace.RootView();
+    }
   });
   
   window.MeepleSpace = new App();
 
+  MeepleSpace.RootView = Backbone.Marionette.LayoutView.extend({
+    el: 'body',
+    regions: {
+      header: '.header',
+      nav: '.nav',
+      footer: '.footer'
+    }
+  });
+
+  MeepleSpace.NavView = Backbone.Marionette.ItemView.extend({
+
+    ui: {
+      nav: '.nav'
+    },
+
+  });
+
+  MeepleSpace.Controller = Backbone.Marionette.Controller.extend({
+    initialize: function () {
+
+    },
+
+    start: function () {
+
+    },
+
+    homeRoute: function () {
+      console.log("test");
+      var _template = 'WHAT!?';
+      var testView = new MeepleSpace.NavView({ template: _.template(_template)});
+      MeepleSpace.rootView.showChildView('.header', testView);
+    }
+
+  });
+
+  MeepleSpace.Router = Backbone.Marionette.AppRouter.extend({
+    appRoutes: {
+      "": "homeRoute"
+    }
+  });
+
+  MeepleSpace.on('before:start', function () {
+    MeepleSpace.setRootLayout();
+  });
+
   MeepleSpace.on('start', function () {
+    MeepleSpace._controller = new MeepleSpace.Controller();
+    MeepleSpace._router = new MeepleSpace.Router({ controller: MeepleSpace._controller });
+    
+    MeepleSpace._controller.start();
     Backbone.history.start();
   });
 
+  MeepleSpace.start();
 })();
